6c9758c4c560290a9b587cf3b027873fa999bf03
XavierGomez <xaviergomezxcv@gmail.com>
2025-10-07 00:40:26 +0200

    chore: initial project setup by smadex

---
+++ app/src/main/kotlin/com/rickandmortyapi/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.rickandmortyapi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // YOUR CODE HERE :)
+    }
+
+    companion object {
+        private const val BASE_URL = "https://rickandmortyapi.com/api/"
+    }
+}
