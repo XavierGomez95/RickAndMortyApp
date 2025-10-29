2393b3c61c89d9db11ef199f98e7961a0d82f181
XavierGomez <xaviergomezxcv@gmail.com>
2025-10-29 20:09:00 +0100

    chore(git): update .gitignore to exclude build and cache files

---
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -0,0 +1,13 @@
+object Dependencies {
+    const val namespace = "com.rickandmortyapi"
+
+    const val applicationId = "com.rickandmortyapi"
+    const val compileSdk = 35
+    const val minSdk = 28
+    const val targetSdk = 34
+
+    const val versionCode = 1
+    const val versionName = "1.0"
+
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+}
